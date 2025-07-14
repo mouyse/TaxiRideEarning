2bf3b039cb31377e74667d26790d062c35f8c34c
Jay Shah <jayy.shah16@gmail.com>
2025-07-14 11:06:29 +0530

    Updated function calls

--- src/index.php
+++ src/index.php
@@ -5,12 +5,12 @@ $pickupLocations = [0, 2, 9, 10, 11, 12];
 $dropLocations = [5, 9, 11, 11, 14, 17];
 $tip = [1, 2, 3, 2, 2, 1];
 
-function taxiDriver($pickups, $drops, $tips){
+function taxiDriver(array $pickups,array $drops,array $tips){
 
     $ride = new TaxiRideEarning\Ride($pickups, $drops, $tips);
     $taxi = new TaxiRideEarning\Taxi($ride);
     
-    return $ride->calculateBestRouteAndEarnings();
+    return $taxi->calculateMaxEarning();
 }
 
 echo "Maximum earnings: ".taxiDriver($pickupLocations, $dropLocations, $tip);
