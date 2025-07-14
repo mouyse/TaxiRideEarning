d545a9170780223050ccc35c886db7fece006e9f
Jay Shah <jayy.shah16@gmail.com>
2025-07-14 11:04:01 +0530

    Updating the function calls

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+require_once '../vendor/autoload.php';
+
+$pickupLocations = [0, 2, 9, 10, 11, 12];
+$dropLocations = [5, 9, 11, 11, 14, 17];
+$tip = [1, 2, 3, 2, 2, 1];
+
+function taxiDriver($pickups, $drops, $tips){
+
+    $ride = new TaxiRideEarning\Ride($pickups, $drops, $tips);
+    $taxi = new TaxiRideEarning\Taxi($ride);
+    
+    return $taxi->calculateMaxEarning();
+}
+
+echo "Maximum earnings: ".taxiDriver($pickupLocations, $dropLocations, $tip);
